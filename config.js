6990b2a8f9ad9a99b853c0a4d3d8450a94642b07
Willymi01 <122224357+Willymi01@users.noreply.github.com>
2026-07-16 18:19:41 +0200

    Version 8.5.1

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.FINANCE_CLOUD_CONFIG = {
-  supabaseUrl: "",
-  supabaseAnonKey: ""
+  supabaseUrl: "https://xefuwxpwrtewbmurzzyn.supabase.co/rest/v1/",
+  supabaseAnonKey: "sb_publishable_XKp2H2_LyFs2GU5vbUzcRw_64bsYF2a"
 };
